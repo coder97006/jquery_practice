254e7d87c22d8e4340d813a8a252034b71089326
Rocky Fatafehi <coder97006@gmail.com>
2019-06-21 21:35:12 -0700

    bootstrap buttons, jquery adding and removing classes

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("button#green").click(function() {
+      $("body").removeClass();
+      $("body").addClass("green-background");
+    });
+  
+    $("button#gold").click(function() {
+      $("body").removeClass();
+      $("body").addClass("gold-background");
+    });
+  
+    $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+    });
+
+    $("button#blue").click(function() {
+      $("body").removeClass();
+      $("body").addClass("blue-background");
+    });
+    
+  });
